1fed3ce35565166fb42af260d3f21d94c7a713c1
Ubuntu-QA <eyal@showbox.com>
2014-10-02 18:34:24 +0300

    stitching

--- background.js
+++ background.js
@@ -4,6 +4,12 @@ function checkForValidUrl(tabId, changeInfo, tab) {
 	if (tab.url.indexOf('maps') !== -1 && tab.url.indexOf('google') !== -1) {
 		chrome.pageAction.show(tabId);
 	}
+
+	if (changeInfo.status == 'complete') {
+		chrome.tabs.executeScript(null, {"file": "injectScript.js"}, function() {
+				console.log('Successfully injected script into the page');
+		});
+	}
 }
 
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
---
+++ injectScript.js
@@ -0,0 +1,7 @@
+// var timer = window.setInterval(function(){
+// console.log($);
+// if($) clearInterval(timer);
+// }, 300);
+window.$(document).ready(function(){
+	console.log("here I am");
+});
